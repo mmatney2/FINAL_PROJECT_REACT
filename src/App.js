f425bc0153de2737270723da18e5c7104643a597
Marquita Matney <marquita.matney@gmail.com>
2022-06-14 14:55:39 -0400

    6-14-22

--- src/App.js
+++ src/App.js
@@ -1,63 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Aztro from './app/Aztro';
+import Button from './components/Button';
 
-class Aztro extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-          json: {}
-        }
-    }
+function App(){
+  return(
+    <>
+    {/* <div>
+      <Button>Submit</Button>
+      </div> */}
+      <div>
+      <Aztro/>
+    </div>
+    </>
+  );
 
-    componentDidMount () {
-        const URL = 'https://aztro.sameerkumar.website/?sign=virgo&day=yesterday';
-        fetch(URL, {
-            method: 'POST'
-        }).then(response => response.json())
-        .then(json => { this.setState({json}); });
-        console.log(this)
-
-    }
-
-    render() {
-        return (
-          <>
-          <div>
-              Current Date: {this.state.json.current_date} <br />
-              Compatibility: {this.state.json.compatibility} <br />
-              Lucky Number: {this.state.json.lucky_number} <br />
-              Lucky Time: {this.state.json.lucky_time} <br />
-              Color: {this.state.json.color} <br />
-              Date Range: {this.state.json.date_range} <br />
-              Mood: {this.state.json.mood} <br />
-              Description: {this.state.json.description} <br />
-          </div>
-          </>
-        );
-    }
 }
-
-export default Aztro;
-
-
-
-
-
-
-
-// function SignDayEntry(){
-//   return(
-//     <div>
-
-//     </div>
-//   )
-// }
-
-// function App() {
-//   return (
-//     <div className="App">
-      
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
--- src/app/Aztro.js
+++ src/app/Aztro.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+
+
 class Aztro extends Component {
     constructor(props){
         super(props);
